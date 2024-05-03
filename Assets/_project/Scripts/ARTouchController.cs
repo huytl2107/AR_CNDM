b9f87c5fb4b4a97e42a193633469e64e4e56e062
Huy <152854861+huytl2107@users.noreply.github.com>
2024-05-03 12:29:48 +0700

    can select and touch only one object

--- Assets/_project/Scripts/ARTouchController.cs
+++ Assets/_project/Scripts/ARTouchController.cs
@@ -8,7 +8,12 @@ public class ARTouchController : MonoBehaviour
     private bool isFingerStationary = true;
     private float timeTouchStarted;
     private LeanFinger trackedFinger;
+    private LeanSelectByFinger leanSelect;
 
+    private void Awake()
+    {
+        leanSelect = GetComponent<LeanSelectByFinger>();
+    }
     private void OnEnable()
     {
         LeanTouch.OnFingerDown += HandleFingerDown;
@@ -28,6 +33,22 @@ public class ARTouchController : MonoBehaviour
         trackedFinger = finger;
         timeTouchStarted = Time.time;
         isFingerStationary = true;
+
+        Ray ray = Camera.main.ScreenPointToRay(finger.ScreenPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            LeanSelectableByFinger selectable = hit.collider.gameObject.GetComponent<LeanSelectableByFinger>();
+            if (selectable != null)
+            {
+                leanSelect.Select(selectable);
+                Debug.Log("Đã chọn GameObject: " + hit.collider.gameObject.name);
+            }
+            else
+            {
+                Debug.Log("Không nhận diện được selectable by finger");
+            }
+        }
     }
 
     private void HandleFingerSet(LeanFinger finger)
@@ -40,6 +61,9 @@ public class ARTouchController : MonoBehaviour
 
     private void HandleFingerUp(LeanFinger finger)
     {
+        //Deselect toàn bộ object được chọn
+        leanSelect.DeselectAll();
+        
         if (finger == trackedFinger && isFingerStationary)
         {
             Ray ray = Camera.main.ScreenPointToRay(finger.ScreenPosition);
@@ -48,6 +72,7 @@ public class ARTouchController : MonoBehaviour
             {
                 if (hit.collider.gameObject.GetComponent<ObjectController>() != null)
                 {
+                    Debug.Log("Hiện info panel");
                     AntiWibu_UIManager.Instance.ShowPanelWithLeanTween("InfoPanel");
                     AntiWibu_UIManager.Instance.ChangeText("InfoPanel", hit.collider.gameObject.GetComponent<ObjectController>().ObjectDataScriptable.ObjectDescription);
                 }
@@ -57,6 +82,8 @@ public class ARTouchController : MonoBehaviour
 
     void Update()
     {
+        //bug chí vcl
+        /*
         if (Input.GetMouseButtonDown(0) )
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -71,5 +98,6 @@ public class ARTouchController : MonoBehaviour
                 }
             }
         }
+        */
     }
 }
